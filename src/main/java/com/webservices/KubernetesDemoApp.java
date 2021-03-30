74f4d8d47427e333d01b31e9b3d678d0cc890f32
sourabh raghav <sourabh.raghav@accenture.com>
2021-03-30 19:51:25 +0700

    Package changed

--- src/main/java/com/webservices/KubernetesDemoApp.java
+++ src/main/java/com/webservices/KubernetesDemoApp.java
@@ -1,10 +1,8 @@
-package com.in28minutes.rest.webservices.restfulwebservices;
+package com.webservices;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
